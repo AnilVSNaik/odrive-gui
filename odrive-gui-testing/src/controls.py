05ea43bdb84aca16d6413195b70c68bea58f5c80
AnilVSNaik <a.sekharnaik@gmail.com>
2023-07-13 17:39:00 +0200

    -Improved efficiency of graphs -Added save funtion should work now

--- odrive-gui-testing/src/controls.py
+++ odrive-gui-testing/src/controls.py
@@ -9,6 +9,13 @@ import asyncio
 import time
 from typing import List
 import threading
+from typing import Optional
+from typing import Tuple
+import matplotlib.pyplot as plt
+
+recording = False
+start_time = None
+data_buffer = []
 
 def controls(odrv):
     modes = {
@@ -34,10 +41,25 @@ def controls(odrv):
         8: 'loop',
     }
 
-    recording = False
-    start_time = None
-    data_buffer = []
+    def update_y_axis_range(checkbox_value):
+        if checkbox_value:
+            # Enable custom range
+            y_axis_range = (custom_y_min.value, custom_y_max.value)
+            # Pass the custom range to the vel_push() function
+            pos_push(y_axis_range)
+            vel_push(y_axis_range)
+            id_push(y_axis_range)
+            iq_push(y_axis_range)
+            t_push(y_axis_range)
+        else:
+            # Disable custom range (use automatic range)
+            pos_push()
+            vel_push()
+            id_push()
+            iq_push()
+            t_push()
 
+    #Begins recording process
     def record_data():
         global recording, start_time, data_buffer
 
@@ -46,24 +68,26 @@ def controls(odrv):
             recording = False
             elapsed_time = datetime.now() - start_time
             print(f"Stopped recording. Elapsed time: {elapsed_time}")
+            recording_button.text = 'Start Recording'
             save_data()
         else:
             # Start recording
             recording = True
             start_time = datetime.now()
-            data_buffer = []
-            print("Started recording.")
-            thread = threading.Thread(target=recording_data)
+            data_buffer = [] #Initiaize the data buffer to fill with data for certain period of time
+            print("Started recording")
+            recording_button.text = 'Stop Recording'
+            thread = threading.Thread(target=recording_data) #Using threads to allow for a number of different methods to run while the save process is working
             thread.start()
 
-    def save_data():
+    def save_data(): #Saves data once the Stop recoding button is pressed
         global data_buffer
 
         if len(data_buffer) > 0:
             filename = f"recorded_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
             with open(filename, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow(['Timestamp', 'Velocity', 'I_G', 'I_D', 'I_Bus'])
+                writer.writerow(['Timestamp', 'Velocity Axis 1', 'I_G Axis 1', 'I_D Axis 1','Velocity Axis 0', 'I_G Axis 0', 'I_D Axis 0', 'I_Bus'])
                 writer.writerows(data_buffer)
 
             print(f"Data saved to {filename}")
@@ -72,110 +96,56 @@ def controls(odrv):
 
     def recording_data():
         global recording, data_buffer
+        #Set for a set interval between data recording
+        sample_interval = 0.001  # Set the desired sample interval in seconds
+        next_sample_time = time.perf_counter() + sample_interval
 
         while recording:
             # Collect data and add it to the buffer
             timestamp = datetime.now()
-            velocity = odrv.axis0.encoder.vel_estimate
-            i_g = odrv.axis0.motor.current_control.Iq_measured
-            i_d = odrv.axis0.motor.current_control.Id_measured
+            velocity_0 = odrv.axis0.encoder.vel_estimate
+            velocity_1 = odrv.axis1.encoder.vel_estimate
+            i_g_0 = odrv.axis0.motor.current_control.Iq_measured
+            i_d_0 = odrv.axis0.motor.current_control.Id_measured
+            i_g_1 = odrv.axis1.motor.current_control.Iq_measured
+            i_d_1 = odrv.axis1.motor.current_control.Id_measured
             i_bus = odrv.ibus
 
-            data_point = [timestamp, velocity, i_g, i_d, i_bus]
+            data_point = [timestamp, velocity_1, i_g_1, i_d_1, velocity_0, i_g_0, i_d_0, i_bus]
             data_buffer.append(data_point)
 
-            # Adjust the sleep duration based on the desired data collection rate
-            time.sleep(0.01)  # Adjust the sleep duration as needed
-    #Implement save start and save end
+            # Calculate the delay until the next sample time
+            delay = next_sample_time - time.perf_counter()
 
-    #Potential for input speeds through txt file or someother
+            if delay > 0:
+                time.sleep(delay)  # Wait for the remaining time until the next sample
 
-    from typing import List
-
-    def format_errors(odrv, clear=False) -> str:
-
-        lines = []
-
-        axes = [(name, getattr(odrv, name)) for name in dir(odrv) if name.startswith('axis')]
-        axes.sort()
-
-        def decode_flags(val, enum_type):
-            errorcodes = {v.value: str(v) for v in enum_type}
-            if val == 0:
-                return "no error"
             else:
-                error_strings = [errorcodes.get((1 << bit), 'UNKNOWN ERROR: 0x{:08X}'.format(1 << bit))
-                                 for bit in range(64) if val & (1 << bit) != 0]
-                return "Error(s): " + ", ".join(error_strings)
+                # If the delay is negative (sampling took longer than expected), print a warning
+                print("Warning: Sampling took longer than the desired interval.")
 
-        def decode_enum(val, enum_type):
-            errorcodes = {v.value: str(v) for v in enum_type}
-            return errorcodes.get(val, 'Unknown value: ' + str(val))
-
-        def decode_drv_fault(axis, val):
-            if val == 0:
-                return "none"
-            elif not hasattr(axis, '_metadata'):
-                return "metadata not loaded"
-            else:
-                return odrive.utils.decode_drv_faults(axis._metadata['drv'], val)
-
-        def dump_item(indent, name, obj, path, decoder):
-            prefix = indent + name.strip('0123456789') + ": "
-            for elem in path.split('.'):
-                if not hasattr(obj, elem):
-                    return ""
-                obj = getattr(obj, elem)
-
-            lines = decoder(obj)
-            lines = [indent + name + ": " + str(lines)] + [
-                indent + "  " + line
-                for line in lines[1:]
-            ]
-            return "\n".join(lines)
-
-        lines.append(dump_item("", "system", odrv, 'error', lambda x: decode_flags(x, odrive.enums.ODriveError)))
-
-        for name, axis in axes:
-            lines.append(name)
-            lines.append(dump_item("  ", 'error', axis, 'error', lambda x: decode_flags(x, odrive.enums.AxisError)))
-            lines.append(dump_item("  ", 'active_errors', axis, 'active_errors',
-                                   lambda x: decode_flags(x, odrive.enums.ODriveError)))
-            lines.append(dump_item("  ", 'disarm_reason', axis, 'disarm_reason',
-                                   lambda x: decode_flags(x, odrive.enums.ODriveError)))
-            lines.append(dump_item("  ", 'procedure_result', axis, 'procedure_result',
-                                   lambda x: decode_enum(x, odrive.enums.ProcedureResult)))
-            lines.append(dump_item("  ", 'last_drv_fault', axis, 'last_drv_fault', lambda x: decode_drv_fault(axis, x)))
-
-        if hasattr(odrv, 'issues') and hasattr(odrv.issues, 'length') and hasattr(odrv.issues, 'get'):
-            if odrv.issues.length == 0:
-                lines.append("internal issues: none")
-            else:
-                issues = [odrv.issues.get(i) for i in range(odrv.issues.length)]
-                lines.append("internal issues: " + str(odrv.issues.length))
-                lines.append("details for bug report: " + str(issues))
-
-        if clear:
-            odrv.clear_errors()
-
-        return "\n".join(lines)
+            next_sample_time += sample_interval
 
     def show_errors():
         errors = str(utils.format_errors(odrv, True))# Get the errors from the ODrive in format but in rich text converted to str
         error_output.set_content(errors)  # Update the output widget with the error information
 
+    #Axis Calibration sequence start
     def axis_calibration(axis):
         axis.requested_state = enums.AxisState.IDLE
         axis.requested_state = enums.AxisState.FULL_CALIBRATION_SEQUENCE
 
-    def start_calibration():
-        print(odrv.axis0)
-        #axis_calibration(odrv.axis0)
-        #time.sleep(30)
+    #Ensures nothing else interrupts the Axis calibration during use
+    def start_calibration_0():
+        axis_calibration(odrv.axis0)
+        while odrv.axis0.current_state != enums.AxisState.IDLE: #Will check if the motor is idle, if is the time.sleep(1) will stop
+            time.sleep(1)
+    def start_calibration_1():
         axis_calibration(odrv.axis1)
-        time.sleep(30)
+        while odrv.axis1.current_state != enums.AxisState.IDLE:
+            time.sleep(1)
 
-    #Add two buttons or 1 button for axis calibration
+    #All components at the top page of the GUI
     with ui.row().classes('items-center'):
         ui.label(f'SN {hex(odrv.serial_number).removeprefix("0x").upper()}')
         ui.label(f'HW {odrv.hw_version_major}.{odrv.hw_version_minor}.{odrv.hw_version_variant}')
@@ -184,12 +154,13 @@ def controls(odrv):
         voltage = ui.label()
         ui.timer(1.0, lambda: voltage.set_text(f'{odrv.vbus_voltage:.2f} V'))
         ui.button(on_click=lambda: odrv.save_configuration()).props('icon=save flat round').tooltip('Save configuration')
-        ui.button("Start/Stop Recording", on_click=record_data).props('icon=record_voice_over flat round')
+        recording_button = ui.button("Start Recording", on_click=record_data).props('icon=record_voice_over flat round')
         ui.button("Show Errors", on_click=show_errors).props('icon=bug_report flat round')
         error_output = ui.markdown()  # Create an output widget for displaying errors
-        ui.button("Axis Calibration", on_click=lambda: start_calibration()).props('icon=build')
+        ui.button("Axis 0 Calibration", on_click=lambda: start_calibration_0()).props('icon=build')
+        ui.button("Axis 1 Calibration", on_click=lambda: start_calibration_1()).props('icon=build')
 
-
+    #Method to bind each button to a function and return commands to the motor controller. Also allows to display important information of the motor
     def axis_column(a: int, axis: Any) -> None:
         ui.markdown(f'### Axis {a}')
 
@@ -280,16 +251,10 @@ def controls(odrv):
             ui.toggle({0: 'axis 0', 1: 'axis 1'}) \
                 .bind_value(ctr_cfg, 'axis_to_mirror') \
                 .bind_visibility_from(input_mode, 'value', value=7)
-        '''async def pos_push() -> None:
-            pos_plot.push([datetime.now()], [[axis.controller.input_pos], [axis.encoder.pos_estimate]])
-            await pos_plot.view.update()
-        pos_check = ui.checkbox('Position plot')
-        pos_plot = ui.line_plot(n=2, update_every=10).with_legend(['input_pos', 'pos_estimate'], loc='upper left', ncol=2)
-        pos_timer = ui.timer(0.05, pos_push)
-        pos_check.bind_value_to(pos_plot, 'visible').bind_value_to(pos_timer, 'active')'''
 
-        #Add autoscale or specify the y-axis and increase efficiency
+        #Allows us to Show the graphs of position, velocity, torque and current
         async def pos_push() -> None:
+            #Allows for the actual recording of the values while other processes take place. All these datapoints are recorded and appended to an empty data buffe which we will then save.
             if recording:
                 timestamp = datetime.now()
                 velocity = axis.encoder.vel_estimate
@@ -303,39 +268,79 @@ def controls(odrv):
             pos_plot.push([datetime.now()], [[axis.controller.input_pos], [axis.encoder.pos_estimate]])
             await pos_plot.view.update()
 
-        async def vel_push() -> None:
+        custom_range_checkbox = ui.checkbox('Custom Y-Axis Range')
+        custom_y_min = ui.number('Y-Min', value=0)
+        custom_y_max = ui.number('Y-Max', value=10)
+
+        # Set the default value of the checkbox to False (no custom range)
+        custom_range_checkbox.value = False
+
+        #All the options for the plots
+        async def vel_push(y_axis_range: Optional[Tuple[float, float]] = None) -> None:
+            '''if y_axis_range is not None:
+                vel_plot.view.set_ylim(*y_axis_range)
+            else:
+                pass'''
             vel_plot.push([datetime.now()], [[axis.controller.input_vel], [axis.encoder.vel_estimate]])
             await vel_plot.view.update()
-        vel_check = ui.checkbox('Velocity plot')
-        vel_plot = ui.line_plot(n=2, update_every=100).with_legend(['input_vel', 'vel_estimate'], loc='upper left', ncol=2)
-        vel_timer = ui.timer(0.05, vel_push)
-        vel_check.bind_value_to(vel_plot, 'visible').bind_value_to(vel_timer, 'active')
 
-        async def id_push() -> None:
+        async def id_push(y_axis_range: Optional[Tuple[float, float]] = None) -> None:
+            #ax = id_plot.view.ax
+            #ax.set_ylim(*y_axis_range)
             id_plot.push([datetime.now()], [[axis.motor.current_control.Id_setpoint], [axis.motor.current_control.Id_measured]])
             await id_plot.view.update()
-        id_check = ui.checkbox('Id plot')
-        id_plot = ui.line_plot(n=2, update_every=100).with_legend(['Id_setpoint', 'Id_measured'], loc='upper left', ncol=2)
-        id_timer = ui.timer(0.05, id_push)
-        id_check.bind_value_to(id_plot, 'visible').bind_value_to(id_timer, 'active')
 
-        async def iq_push() -> None:
+        async def iq_push(y_axis_range: Optional[Tuple[float, float]] = None) -> None:
+            #ax = iq_plot.view.ax
+            #ax.set_ylim(*y_axis_range)
             iq_plot.push([datetime.now()], [[axis.motor.current_control.Iq_setpoint], [axis.motor.current_control.Iq_measured]])
             await iq_plot.view.update()
-        iq_check = ui.checkbox('Iq plot')
-        iq_plot = ui.line_plot(n=2, update_every=100).with_legend(['Iq_setpoint', 'Iq_measured'], loc='upper left', ncol=2)
-        iq_timer = ui.timer(0.05, iq_push)
-        iq_check.bind_value_to(iq_plot, 'visible').bind_value_to(iq_timer, 'active')
 
-        async def t_push() -> None:
+        async def t_push(y_axis_range: Optional[Tuple[float, float]] = None) -> None:
+            #ax = t_plot.view.ax
+            #ax.set_ylim(*y_axis_range)
             t_plot.push([datetime.now()], [[axis.motor.fet_thermistor.temperature]])
             await t_plot.view.update()
-        t_check = ui.checkbox('Temperature plot')
-        t_plot = ui.line_plot(n=1, update_every=100)
-        t_timer = ui.timer(0.05, t_push)
-        t_check.bind_value_to(t_plot, 'visible').bind_value_to(t_timer, 'active')
+
+
+        #Characteristics of the plots
+        with ui.row():
+            pos_check = ui.checkbox('Position plot')
+            pos_plot = ui.line_plot(n=2, update_every=0.125).with_legend(['input_pos', 'pos_estimate'], loc='upper left', ncol=2)
+            pos_timer = ui.timer(0.05, pos_push)
+            pos_check.bind_value_to(pos_plot, 'visible').bind_value_to(pos_timer, 'active')
+
+            vel_check = ui.checkbox('Velocity plot')
+            vel_plot = ui.line_plot(n=2, update_every=0.125).with_legend(['input_vel', 'vel_estimate'], loc='upper left', ncol=2)
+            vel_timer = ui.timer(0.05, vel_push)
+            vel_check.bind_value_to(vel_plot, 'visible').bind_value_to(vel_timer, 'active')
+
+            id_check = ui.checkbox('Id plot')
+            id_plot = ui.line_plot(n=2, update_every=0.125).with_legend(['Id_setpoint', 'Id_measured'], loc='upper left', ncol=2)
+            id_timer = ui.timer(0.05, id_push)
+            id_check.bind_value_to(id_plot, 'visible').bind_value_to(id_timer, 'active')
+
+            iq_check = ui.checkbox('Iq plot')
+            iq_plot = ui.line_plot(n=2, update_every=0.125).with_legend(['Iq_setpoint', 'Iq_measured'], loc='upper left', ncol=2)
+            iq_timer = ui.timer(0.05, iq_push)
+            iq_check.bind_value_to(iq_plot, 'visible').bind_value_to(iq_timer, 'active')
+
+            t_check = ui.checkbox('Temperature plot')
+            t_plot = ui.line_plot(n=1, update_every=0.125)
+            t_timer = ui.timer(0.05, t_push)
+            t_check.bind_value_to(t_plot, 'visible').bind_value_to(t_timer, 'active')
+
+        #custom_range_checkbox.bind_value_to(vel_plot.view, 'ylim')
 
     with ui.row():
         for a, axis in enumerate([odrv.axis0, odrv.axis1]):
             with ui.card(), ui.column():
                 axis_column(a, axis)
+
+
+
+
+
+
+
+
